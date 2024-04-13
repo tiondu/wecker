b5099d7c03627619638b4d1a18f9b1323cc0bb23
tiondu <166902487+tiondu@users.noreply.github.com>
2024-04-13 21:48:46 +0200

    Wecker um 7:15 mit Startzeit 21:48 Uhr

--- main.ts
+++ main.ts
@@ -5,10 +5,10 @@ DS1307.stop()
 DS1307.DateTime(
 2024,
 4,
-15,
+13,
 4,
-15,
-1,
+21,
+48,
 0
 )
 DS1307.start()
@@ -25,8 +25,8 @@ basic.forever(function () {
         Minuten = convertToText(DS1307.getMinute())
     }
     _4digit.show(parseFloat("" + Stunden + Minuten))
-    if (DS1307.getHour() == 15 && DS1307.getMinute() == 5) {
-        for (let index = 0; index < 10; index++) {
+    if (DS1307.getHour() == 7 && DS1307.getMinute() == 15) {
+        for (let index = 0; index < 24; index++) {
             music.play(music.stringPlayable("C D E F G A B C5 ", 140), music.PlaybackMode.UntilDone)
         }
     }
