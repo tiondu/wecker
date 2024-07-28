3fe54dfca8194b16d15c6b969c5f0cc6c4cd33ff
tiondu <166902487+tiondu@users.noreply.github.com>
2024-07-28 13:13:43 +0200

    Uhrzeit-Block entfernt

--- main.ts
+++ main.ts
@@ -2,20 +2,9 @@ let Minuten = ""
 let Stunden = ""
 lcd16x2rgb.initLCD(lcd16x2rgb.lcd16x2_eADDR(lcd16x2rgb.eADDR_LCD.LCD_16x2))
 lcd16x2rgb.writeText(lcd16x2rgb.lcd16x2_eADDR(lcd16x2rgb.eADDR_LCD.LCD_16x2), 0, 0, 15, lcd16x2rgb.lcd16x2_text("Wecker klingelt"))
-lcd16x2rgb.writeText(lcd16x2rgb.lcd16x2_eADDR(lcd16x2rgb.eADDR_LCD.LCD_16x2), 1, 0, 15, lcd16x2rgb.lcd16x2_text("um 7:00 Uhr"))
+lcd16x2rgb.writeText(lcd16x2rgb.lcd16x2_eADDR(lcd16x2rgb.eADDR_LCD.LCD_16x2), 1, 0, 15, lcd16x2rgb.lcd16x2_text("um 13:35 Uhr"))
 let _4digit = grove.createDisplay(DigitalPin.C16, DigitalPin.C17)
 _4digit.point(true)
-DS1307.stop()
-DS1307.DateTime(
-2024,
-5,
-3,
-5,
-20,
-29,
-10
-)
-DS1307.start()
 basic.forever(function () {
     if (DS1307.getHour() < 10) {
         Stunden = "0" + DS1307.getHour()
@@ -28,9 +17,9 @@ basic.forever(function () {
         Minuten = convertToText(DS1307.getMinute())
     }
     _4digit.show(parseFloat("" + Stunden + Minuten))
-    if (DS1307.getHour() == 7 && DS1307.getMinute() == 0) {
+    if (DS1307.getHour() == 13 && DS1307.getMinute() == 35) {
         lcd16x2rgb.writeText(lcd16x2rgb.lcd16x2_eADDR(lcd16x2rgb.eADDR_LCD.LCD_16x2), 0, 0, 15, lcd16x2rgb.lcd16x2_text("Jonathan"))
-        lcd16x2rgb.writeText(lcd16x2rgb.lcd16x2_eADDR(lcd16x2rgb.eADDR_LCD.LCD_16x2), 1, 0, 15, lcd16x2rgb.lcd16x2_text("aufstehen!"))
+        lcd16x2rgb.writeText(lcd16x2rgb.lcd16x2_eADDR(lcd16x2rgb.eADDR_LCD.LCD_16x2), 1, 0, 15, lcd16x2rgb.lcd16x2_text("es gibt Essen!"))
         for (let index = 0; index < 24; index++) {
             music.play(music.stringPlayable("C D E F G A B C5 ", 140), music.PlaybackMode.UntilDone)
         }
