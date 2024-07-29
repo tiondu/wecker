2fd81a1328081975a6aa080c59db3594b46e0e1b
tiondu <166902487+tiondu@users.noreply.github.com>
2024-07-29 19:16:17 +0200

    Weckzeit Berechnung

--- main.ts
+++ main.ts
@@ -1,7 +1,27 @@
+input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
+    Weckzeit_Berechnung = DS1307.getHour() + 10
+    if (Weckzeit_Berechnung >= 24) {
+        Weckzeit_Berechnung = Weckzeit_Berechnung - 24
+    }
+    if (Weckzeit_Berechnung < 10) {
+        Weckzeit_Stunde = "0" + convertToText(Weckzeit_Berechnung)
+    } else {
+        Weckzeit_Stunde = convertToText(Weckzeit_Berechnung)
+    }
+    if (DS1307.getMinute() < 10) {
+        Weckzeit_Minute = "0" + convertToText(DS1307.getMinute())
+    } else {
+        Weckzeit_Minute = convertToText(DS1307.getMinute())
+    }
+    lcd16x2rgb.writeText(lcd16x2rgb.lcd16x2_eADDR(lcd16x2rgb.eADDR_LCD.LCD_16x2), 1, 0, 15, "um " + Weckzeit_Stunde + ":" + Weckzeit_Minute + " Uhr")
+})
 let Minuten = ""
 let Stunden = ""
-let Weckzeit_Stunde = convertToText(18)
-let Weckzeit_Minute = convertToText(55)
+let Weckzeit_Berechnung = 0
+let Weckzeit_Minute = ""
+let Weckzeit_Stunde = ""
+Weckzeit_Stunde = convertToText(18)
+Weckzeit_Minute = convertToText(55)
 lcd16x2rgb.initLCD(lcd16x2rgb.lcd16x2_eADDR(lcd16x2rgb.eADDR_LCD.LCD_16x2))
 lcd16x2rgb.writeText(lcd16x2rgb.lcd16x2_eADDR(lcd16x2rgb.eADDR_LCD.LCD_16x2), 0, 0, 15, lcd16x2rgb.lcd16x2_text("Wecker klingelt"))
 lcd16x2rgb.writeText(lcd16x2rgb.lcd16x2_eADDR(lcd16x2rgb.eADDR_LCD.LCD_16x2), 1, 0, 15, "um " + Weckzeit_Stunde + ":" + Weckzeit_Minute + " Uhr")
@@ -21,7 +41,7 @@ basic.forever(function () {
     _4digit.show(parseFloat("" + Stunden + Minuten))
     if (Stunden == Weckzeit_Stunde && Minuten == Weckzeit_Minute) {
         lcd16x2rgb.writeText(lcd16x2rgb.lcd16x2_eADDR(lcd16x2rgb.eADDR_LCD.LCD_16x2), 0, 0, 15, lcd16x2rgb.lcd16x2_text("Jonathan"))
-        lcd16x2rgb.writeText(lcd16x2rgb.lcd16x2_eADDR(lcd16x2rgb.eADDR_LCD.LCD_16x2), 1, 0, 15, lcd16x2rgb.lcd16x2_text("es gibt Essen!"))
+        lcd16x2rgb.writeText(lcd16x2rgb.lcd16x2_eADDR(lcd16x2rgb.eADDR_LCD.LCD_16x2), 1, 0, 15, lcd16x2rgb.lcd16x2_text("aufstehen!"))
         for (let index = 0; index < 24; index++) {
             music.play(music.stringPlayable("C D E F G A B C5 ", 140), music.PlaybackMode.UntilDone)
         }
