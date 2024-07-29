3a1bb0f9cc415b96707b886ed6cddb9bacb265f4
tiondu <166902487+tiondu@users.noreply.github.com>
2024-07-29 18:51:48 +0200

    Weckzeit als Variable

--- main.ts
+++ main.ts
@@ -1,8 +1,10 @@
 let Minuten = ""
 let Stunden = ""
+let Weckzeit_Stunde = convertToText(18)
+let Weckzeit_Minute = convertToText(55)
 lcd16x2rgb.initLCD(lcd16x2rgb.lcd16x2_eADDR(lcd16x2rgb.eADDR_LCD.LCD_16x2))
 lcd16x2rgb.writeText(lcd16x2rgb.lcd16x2_eADDR(lcd16x2rgb.eADDR_LCD.LCD_16x2), 0, 0, 15, lcd16x2rgb.lcd16x2_text("Wecker klingelt"))
-lcd16x2rgb.writeText(lcd16x2rgb.lcd16x2_eADDR(lcd16x2rgb.eADDR_LCD.LCD_16x2), 1, 0, 15, lcd16x2rgb.lcd16x2_text("um 13:35 Uhr"))
+lcd16x2rgb.writeText(lcd16x2rgb.lcd16x2_eADDR(lcd16x2rgb.eADDR_LCD.LCD_16x2), 1, 0, 15, "um " + Weckzeit_Stunde + ":" + Weckzeit_Minute + " Uhr")
 let _4digit = grove.createDisplay(DigitalPin.C16, DigitalPin.C17)
 _4digit.point(true)
 basic.forever(function () {
@@ -17,7 +19,7 @@ basic.forever(function () {
         Minuten = convertToText(DS1307.getMinute())
     }
     _4digit.show(parseFloat("" + Stunden + Minuten))
-    if (DS1307.getHour() == 13 && DS1307.getMinute() == 35) {
+    if (Stunden == Weckzeit_Stunde && Minuten == Weckzeit_Minute) {
         lcd16x2rgb.writeText(lcd16x2rgb.lcd16x2_eADDR(lcd16x2rgb.eADDR_LCD.LCD_16x2), 0, 0, 15, lcd16x2rgb.lcd16x2_text("Jonathan"))
         lcd16x2rgb.writeText(lcd16x2rgb.lcd16x2_eADDR(lcd16x2rgb.eADDR_LCD.LCD_16x2), 1, 0, 15, lcd16x2rgb.lcd16x2_text("es gibt Essen!"))
         for (let index = 0; index < 24; index++) {
